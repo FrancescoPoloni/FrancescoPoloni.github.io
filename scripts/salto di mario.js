fc430c3a56ec64d6c9de824548599474b986b907
FrancescoPoloni <150195514+FrancescoPoloni@users.noreply.github.com>
2023-11-07 23:16:20 +0100

    Add files via upload

---
+++ scripts/salto di mario.js
@@ -0,0 +1,7 @@
+
+function scambiaImmagine(){
+    var myImage = document.getElementById("marioImmagine");
+    var testoSalto = document.getElementById("testoSalto");
+    myImage.src="../assets/mario%20jump.gif";
+    testoSalto.style.display = "none";
+}
